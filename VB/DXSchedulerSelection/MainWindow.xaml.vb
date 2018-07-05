ea90cb13d564e808383d92786995ac52878ed251
DevExpressExampleBot <fakeEmail>
2018-07-05 18:51:52 +0300

    Source auto update [skip ci]

---
+++ VB/DXSchedulerSelection/MainWindow.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Scheduling
+Imports System.Windows
+
+Namespace DXSchedulerSelection
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub schedulerControl_DependencyPropertyChanged(ByVal sender As Object, ByVal e As DependencyPropertyChangedEventArgs)
+            Dim scheduler As SchedulerControl = TryCast(sender, SchedulerControl)
+            If e.Property Is SchedulerControl.SelectedIntervalProperty Then
+                OutputBox.AppendText(String.Format("SelectedInterval is changed. New value is {0} " & ControlChars.CrLf, e.NewValue))
+                OutputBox.ScrollToEnd()
+            End If
+            If e.Property Is SchedulerControl.SelectedResourceProperty Then
+                OutputBox.AppendText(String.Format("SelectedResource is changed. New value is {0} " & ControlChars.CrLf, DirectCast(e.NewValue, ResourceItem).Caption))
+                OutputBox.ScrollToEnd()
+            End If
+        End Sub
+    End Class
+End Namespace
---
+++ VB/DXSchedulerSelection/ViewModel/MainViewModel.vb
@@ -0,0 +1,28 @@
+﻿#Region "#viewmodel"
+Imports DevExpress.Mvvm.DataAnnotations
+Imports System.Collections.ObjectModel
+
+Namespace DXSchedulerSelection
+    <POCOViewModel> _
+    Public Class MainViewModel
+        Private ReadOnly data As New SportChannelsData(3)
+        Protected Sub New()
+        End Sub
+        Public Overridable ReadOnly Property SportEvents() As ObservableCollection(Of SportEvent)
+            Get
+                Return data.Events
+            End Get
+        End Property
+        Public Overridable ReadOnly Property SportChannels() As ObservableCollection(Of SportChannel)
+            Get
+                Return data.Channels
+            End Get
+        End Property
+        Public Overridable ReadOnly Property SportGroups() As ObservableCollection(Of SportGroup)
+            Get
+                Return data.Groups
+            End Get
+        End Property
+    End Class
+End Namespace
+#End Region ' #viewmodel
